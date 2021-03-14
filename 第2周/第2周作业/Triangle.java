071fbc7ef485d5fc54b193f8394372620a3e3074
Hanmycode <71752865+Hanmycode@users.noreply.github.com>
2021-03-15 01:15:09 +0800

    The homework of week 2.

--- 第2周/第2周作业/Triangle.java
+++ 第2周/第2周作业/Triangle.java
@@ -6,14 +6,15 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		int n;
+		System.out.print("ÇëÊäÈëĞĞÊı:");
 		Scanner scan = new Scanner(System.in);
 		n = scan.nextInt();
-		for(int i=1;i<=n;i++) {
-			
-			for(int k=1;k<=n-i;k++)
+		for (int i = 1; i <= n; i++) {
+
+			for (int k = 1; k <= n - i; k++)
 				System.out.print(" ");
-			
-			for (int j = 1; j <= i; j++) 
+
+			for (int j = 1; j <= i; j++)
 				System.out.print("* ");
 
 			System.out.println();
